2149d9056958ff681910f355fcd237d023245928
Houzsaad <227036578+Houzsaad@users.noreply.github.com>
2026-06-18 19:56:38 -0900

    built: permission boder, that controls what users camn do

---
+++ vip/permissions.py
@@ -0,0 +1,19 @@
+from rest_framework import permissions
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+        Read access for anyone authenticated.
+        Write access only for the object's owner or an admin
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any authenticated user,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        if request.user.is_admin:
+            return True
+
+        return obj.owner == request.user
+       
