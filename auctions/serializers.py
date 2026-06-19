af00ac667472827a217047528c9d6edd19963729
Houzsaad <227036578+Houzsaad@users.noreply.github.com>
2026-06-19 03:15:02 -0900

    built serializers that handle all the main app logic

--- auctions/serializers.py
+++ auctions/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Listing
+from .models import Listing, Bid
 
 class ListingSerializer(serializers.ModelSerializer):
     owner = serializers.StringRelatedField(read_only=True)
@@ -28,3 +28,49 @@ class ListingSerializer(serializers.ModelSerializer):
         validated_data['owner'] = self.context['request'].user
         validated_data['current_price'] = validated_data['starting_price']
         return super().create(validated_data)
+
+
+
+class BidSerializer(serializers.ModelSerializer):
+    bidder = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Bid
+        fields = [
+            'id',
+            'listing',
+            'bidder',
+            'amount',
+            'created_at',
+        ]
+        read_only_fields = ['bidder', 'created_at']
+
+    def validate_amount(self, data):
+        request = self.context['request']
+        listing = self.context['listing']
+
+        listing.close_if_expired() #lazy close check happen here tooo
+
+        if listing.status != listing.Status.ACTIVE:
+            raise serializers.ValidationError("This auction is no longer active.")
+
+        if listing.owner == request.user:
+            raise serializers.ValidationError("You cannot bid on your own Auction.")
+
+        if data['amount'] <= listing.current_price:
+            raise serializers.ValidationError(
+                f"Bid must be higher than the current price ({listing.current_price})"
+            )
+
+        return data
+
+    def create(self, validated_data):
+        validated_data["bidder"] = self.context["request"].user
+        bid = super().create(validated_data)
+
+        #update the current price of the listing if the bid is valid
+        listing = bid.listing
+        bid.current_price = bid.amount
+        listing.save(update_fields=["current_price"])
+
+        return bid
